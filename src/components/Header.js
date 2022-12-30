042edb40bc5a4145648df9f29c78238ab70c7b6c
Sushil <102742394+SushilBhandary@users.noreply.github.com>
2022-12-30 21:10:42 +0530

    updateing heater fount Size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 
 const Header = () => {
-    return(
-      <header class="text-gray-600 body-font bg-[#CAD5E2]">
-        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
-          <div>Task to Complete : 5</div>
-          <div> My TODO APPLICATION</div>
-          <div><div className="rounded-full bg-rose-600 p-9"></div> UserName </div>
-        </div>
-      </header>
-    )
-  }
+  return(
+    <header className="text-gray-600 body-font bg-[#CAD5E2]">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+        <div className="text-2xl">Task to Complete : 5</div>
+        <div className="text-3xl"> My TODO APPLICATION</div>
+        <div className="text-xl"><div className="rounded-full bg-rose-600 p-9"></div> UserName </div>
+      </div>
+    </header>
+  )
+}
 
 
 export default Header
