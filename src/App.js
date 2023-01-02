1e219a0b07d38f7eb9517522de7c55a17634f44b
Sushil <102742394+SushilBhandary@users.noreply.github.com>
2023-01-02 20:42:03 +0530

    adding login and singup logic part-1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-// import Login from "./components/login";
-// import Signup from "./components/signup";
+import { useState } from "react";
+import Login from "./components/login";
+import Signup from "./components/signup";
 import TodoPage from "./components/TodoPage";
 
 function App() {
+
+  const [islogin, SetLogin] = useState(false)
+  const [toSingup, setToSingup] = useState(false)
+
   return (
     <div >
-      <TodoPage/ >
+      {!true ? ( toSingup ? <Signup setToSingup={setToSingup}/> : <Login setToSingup={setToSingup}/>) : (
+        <TodoPage/>
+      )}
     </div>
   );
 }
